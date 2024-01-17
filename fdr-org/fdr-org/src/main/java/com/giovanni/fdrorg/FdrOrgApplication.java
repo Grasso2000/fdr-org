776a73f53659a1046eae03cc8f889fce37f23cc0
Grasso2000 <134400651+Grasso2000@users.noreply.github.com>
2024-01-17 11:34:01 +0100

    Add files via upload

---
+++ fdr-org/fdr-org/src/main/java/com/giovanni/fdrorg/FdrOrgApplication.java
@@ -0,0 +1,13 @@
+package com.giovanni.fdrorg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FdrOrgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FdrOrgApplication.class, args);
+	}
+
+}
---
+++ fdr-org/fdr-org/src/test/java/com/giovanni/fdrorg/FdrOrgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giovanni.fdrorg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FdrOrgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
